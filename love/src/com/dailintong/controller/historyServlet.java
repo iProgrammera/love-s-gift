5128d49aa9003507fa029809d531ce4d772e68b7
Tony Stark <dailintong_work@163.com>
2017-12-01 01:46:38 +0800

    增加图片库链接
    增加回忆图片展示页面

---
+++ love/src/com/dailintong/controller/historyServlet.java
@@ -0,0 +1,19 @@
+package com.dailintong.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/history")
+public class historyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String path = request.getContextPath();
+		String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+		response.sendRedirect(basePath + "history/index.html");
+	}
+}
